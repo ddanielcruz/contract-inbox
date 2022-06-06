b2deb6e88141e637693ec2a15917dfb8e671cc08
Daniel Cruz <danielcunha54@gmail.com>
2022-06-06 18:26:33 -0300

    Deploy contract to Rinkeby network

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Contract Inbox
 
-Ethereum application developed during a Solidity course.
+Ethereum application developed during a Solidity course. A sample contract was deployed to **Rinkeby network** at address `0xfa9C8D5Aa125e2b309CAE797ab940ecc1Fd6F9cc`.
--- deploy.js
+++ deploy.js
@@ -1,1 +1,23 @@
-// deploy code will go here
+require('dotenv/config')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+
+const { interface, bytecode } = require('./compile')
+
+const provider = new HDWalletProvider(
+  process.env.METAMASK_SECRET,
+  'https://rinkeby.infura.io/v3/0df7c06aaaaf494fb1c1f9fb3ff308ff'
+)
+const web3 = new Web3(provider)
+
+async function deploy() {
+  const [, account] = await web3.eth.getAccounts()
+  const contract = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['What is life?'] })
+    .send({ gas: '1000000', from: account })
+
+  console.log(`Contract deployed to`, contract.options.address)
+  provider.engine.stop()
+}
+
+deploy()
