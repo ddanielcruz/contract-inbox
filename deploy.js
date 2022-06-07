b87c731e7de0df6a4c3e204933bc95a28f0b5e20
Daniel Cruz <danielcunha54@gmail.com>
2022-06-07 09:53:00 -0300

    Update environment variable name

--- deploy.js
+++ deploy.js
@@ -5,7 +5,7 @@ const Web3 = require('web3')
 const { interface, bytecode } = require('./compile')
 
 const provider = new HDWalletProvider(
-  process.env.METAMASK_SECRET,
+  process.env.ACCOUNT_SECRET,
   'https://rinkeby.infura.io/v3/0df7c06aaaaf494fb1c1f9fb3ff308ff'
 )
 const web3 = new Web3(provider)
